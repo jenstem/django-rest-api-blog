eef9f9c65062c5caaea1930c8c4909c57f9e038d
Jen <jennifer.stemkowski@gmail.com>
2024-03-18 14:20:36 -0400

    Override HTTP method delete

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('blogposts/', views.BlogPostListCreate.as_view(), name="blogpost-view-create" ),
+    path("blogposts/<int:pk>/", views.BlogPostRetrieveUpdateDestroy.as_view(), name="blogpost-view-update-delete"),
+    path("blogposts/", views.BlogPostList.as_view(), name="blogpost-view-list"),
 ]
