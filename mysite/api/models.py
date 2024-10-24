e8f0a75af6a3321bc484468c59e7d2e8cd5f6180
Jen <124745888+jenstem@users.noreply.github.com>
2024-10-24 15:12:23 -0400

    Update docstring in models.py

--- mysite/api/models.py
+++ mysite/api/models.py
@@ -1,9 +1,23 @@
 from django.db import models
 
 class BlogPost(models.Model):
+    """
+    A model representing a blog post.
+
+    Attributes:
+        title (str): The title of the blog post.
+        content (str): The content of the blog post.
+        published_date (datetime): The date and time when the blog post was published.
+    """
     title = models.CharField(max_length=100)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
+        """
+        Returns the string representation of the BlogPost instance.
+
+        Returns:
+            str: The title of the blog post.
+        """
         return self.title
