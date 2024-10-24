9fa5427918512bbb24e6099175483490947d8af2
Jen <124745888+jenstem@users.noreply.github.com>
2024-10-24 15:13:38 -0400

    Update docstring in apps.py

--- mysite/api/apps.py
+++ mysite/api/apps.py
@@ -2,5 +2,12 @@ from django.apps import AppConfig
 
 
 class ApiConfig(AppConfig):
+    """
+    Configuration class for the 'api' application in Django project.
+
+    This class inherits from AppConfig and is responsible for setting up
+    application-specific configurations, such as the default primary key
+    field type.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
